c23ac9dafab9743520bcca1af3e5812e6954d3a3
phall168k <phall168k@gmail.com>
2026-08-13 09:30:02 +0700

    feat: add navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,40 @@
 <template>
   <div>
+    <nav class="w-full h-16 border-b fixed flex items-center justify-center">
+      <div class="w-[90%] h-full flex items-center justify-between">
+        <NuxtLink to="/" class="flex items-center gap-2">
+          <img class="h-10" src="/images/logo.png"/>
+          <div>Chat App</div>
+        </NuxtLink>
+        <div class="flex items-center gap-2">
+          <div class="w-[40px] h-[40px] bg-gray-200 text-gray-700 rounded-full flex items-center justify-center relative">
+            <Icon :size="25" name="ri:notification-line"/>
+            <div class="bg-red-500 text-white w-[15px] h-[15px] rounded-md text-[12px] absolute -top-0 -right-0 flex items-center justify-center px-2">9+</div>
+          </div>
+          <el-dropdown>
+            <el-image 
+              src="https://hzgtzadaduhyxqahebvu.supabase.co/storage/v1/object/public/fashion-images/products/ce80885b-c7b6-4d4f-9dc8-d26ecdd5bfb5.jpg"
+              class="h-[40px] w-[40px] rounded-full"
+              fit="cover"
+            />
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item>
+                  <Icon name="mingcute:profile-line" :size="20"/>&nbsp;&nbsp;Edit Profile
+                </el-dropdown-item>
+                <el-dropdown-item>
+                  <Icon name="meteor-icons:lock" :size="20"/>&nbsp;&nbsp;Change Password
+                </el-dropdown-item>
+                <el-dropdown-item>
+                  <Icon name="ant-design:logout-outlined" :size="20"/>&nbsp;&nbsp;Logout
+                </el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+          
+        </div>
+      </div>
+    </nav>
     <slot />
   </div>
 </template>
